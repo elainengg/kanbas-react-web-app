70593db29fce052b8b5d691b464419a253546bfb
Elaine <128241985+elainengg@users.noreply.github.com>
2024-09-22 13:32:04 -0400

    still need to do formatting for assignment edits

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -6,6 +6,7 @@ import Courses from "./Courses";
 export default function Kanbas() {
   return (
     <div id="wd-kanbas">
+      <h1>Kanbas</h1>
       <table>
         <tr>
           <td valign="top">
@@ -14,6 +15,7 @@ export default function Kanbas() {
           <td valign="top">
             <Routes>
               <Route path="/" element={<Navigate to="Account" />} />
+              
               <Route path="/Account/*" element={<Account />} />
               <Route path="/Dashboard" element={<Dashboard />} />
               <Route path="/Courses/:cid/*" element={<Courses />} />
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,7 @@ import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div id="wd-labs">
+      <h1> Elaine Ng</h1>
       <h1>Labs</h1>
     
       <TOC />
@@ -15,7 +16,9 @@ export default function Labs() {
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
+        
       </Routes>
+
     </div>
 );
 
