89abbd3e1d772a9b58b4d8f6f1f299b107b4f0c3
Elaine <128241985+elainengg@users.noreply.github.com>
2024-09-22 03:20:50 -0400

    Assignment Screen 3.7. I still need to implement the dashboard.tsx in 3.3

---
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -0,0 +1,45 @@
+export default function Assignments() {
+    return (
+      <div id="wd-assignments">
+        <input id="wd-search-assignment"
+               placeholder="Search for Assignments" />
+        <button id="wd-add-assignment-group">+ Group</button>
+        <button id="wd-add-assignment">+ Assignment</button>
+        <h3 id="wd-assignments-title">
+          ASSIGNMENTS 40% of Total <button>+</button>
+        </h3>
+        <ul id="wd-assignment-list">
+          <li className="wd-assignment-list-item">
+            <a className="wd-assignment-link"
+              href="#/Kanbas/Courses/1234/Assignments/123">
+              A1 - ENV + HTML <br></br>
+            </a>
+            Multiple Modules | <b> Not Available until</b> May 6 at 12:00am | <br></br>
+             <b> Due</b> May 13 at 11:59pm | 100 pts <br></br>
+          </li>
+
+
+          <li className="wd-assignment-list-item">
+            {/* Complete On Your Own */}
+            <a className="wd-assignment-link"
+              href="#/Kanbas/Courses/1234/Assignments/123">
+              A2 - CSS + BOOTSTRAP <br></br>
+            </a>
+            Multiple Modules | <b> Not Available until</b> May 13 at 12:00am | <br></br>
+             <b> Due</b> May 20 at 11:59pm | 100 pts <br></br>
+          </li>
+
+
+          <li className="wd-assignment-list-item">
+            {/* Complete On Your Own */}
+            <a className="wd-assignment-link"
+              href="#/Kanbas/Courses/1234/Assignments/123">
+              A3 - JAVASCRIPT + REACT <br></br>
+            </a>
+            Multiple Modules | <b> Not Available until</b> May 20 at 12:00am |<br></br>
+             <b> Due</b> May 27 at 11:59pm | 100 pts<br></br>
+          </li>
+        </ul>
+      </div>
+  );}
+  
--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -4,13 +4,13 @@ export default function CourseStatus() {
         <h2>Course Status</h2>
     
         <button>Unpublish</button> 
-        <button>Publish</button>
+        <button>Publish</button> <br></br>
         {/* Complete on your own */}
-        <button>Import Existing Content</button>
-        <button>Import from Commons</button>
-        <button>View Course Stream</button>
-        <button>New Announcement</button>
-        <button>New Analytics</button>
+        <button>Import Existing Content</button><br></br>
+        <button>Import from Commons</button><br></br>
+        <button>View Course Stream</button><br></br>
+        <button>New Announcement</button><br></br>
+        <button>New Analytics</button><br></br>
 
 
 
