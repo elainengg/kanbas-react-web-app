5b184294dbafdc16c2a1b2a86211aa84999b7403
Elaine <128241985+elainengg@users.noreply.github.com>
2024-10-05 22:55:29 -0400

    3.3.17 start with

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -13,9 +13,18 @@ import GridLayout from "./GridLayout";
 import Flex from "./Flex"; 
 import ReactIcons from "./ReactIcons"; 
 
+import BootstrapGrids from "./BootstrapGrids"; 
+import ScreenSizeLabel from "./ScreenSizeLabel"; 
+import BootstrapTables from "./BootstrapTables"; 
+import BootstrapLists from "./BootstrapLists"; 
+
+import BootstrapForms from "./BootstrapForms"; 
+import BootstrapNavigation from "./BootstrapNavigation"; 
+
 
 export default function Lab2() {
   return (
+    <div className="container">
     <div id="wd-lab2">
       <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>
@@ -86,10 +95,21 @@ export default function Lab2() {
       <GridLayout />
       <Flex />
       <ReactIcons />
+      <BootstrapGrids />
+      <ScreenSizeLabel />
+
+      <BootstrapGrids />
+      <ScreenSizeLabel />
+      <BootstrapTables />
+      <BootstrapLists />
+      <BootstrapForms />
+      <BootstrapNavigation />
+      
 
 
 
 
+    </div>
     </div>
   );
 }
