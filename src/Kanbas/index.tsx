719196ecdda371df8870be204ff49329ca994c00
Elaine <128241985+elainengg@users.noreply.github.com>
2024-10-06 01:13:11 -0400

    4.0

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -7,12 +7,9 @@ export default function Kanbas() {
   return (
     <div id="wd-kanbas">
       <h1>Kanbas</h1>
-      <table>
-        <tr>
-          <td valign="top">
             <KanbasNavigation />
-          </td>
-          <td valign="top">
+            <div>
+
             <Routes>
               <Route path="/" element={<Navigate to="Account" />} />
               <Route path="/Account/*" element={<Account />} />
@@ -21,9 +18,8 @@ export default function Kanbas() {
               <Route path="/Calendar" element={<h1>Calendar</h1>} />
               <Route path="/Inbox" element={<h1>Inbox</h1>} />
             </Routes>
-          </td>
-        </tr>
-      </table>
-    </div>
+            </div>
+            </div>
+
   );
 }
