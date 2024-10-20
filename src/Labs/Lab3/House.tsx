4765f6a956b45f907b931782b7e7a896931b04ec
Elaine <128241985+elainengg@users.noreply.github.com>
2024-10-19 20:30:48 -0400

    up to 2.4

---
+++ src/Labs/Lab3/House.tsx
@@ -0,0 +1,38 @@
+// objects
+// Number, String, Boolean, arrays and other objects
+
+export default function House() {
+    const house = {
+        // this is our house
+      bedrooms: 4,
+      bathrooms: 2.5,
+      squareFeet: 2000,
+
+      // this is the address 
+      address: {
+        street: "Via Roma",
+        city: "Roma",
+        state: "RM",
+        zip: "00100",
+        country: "Italy",
+      },
+      // these are the owners
+      owners: ["Alice", "Bob"],
+    };
+    return (
+        // our house 
+      <div id="wd-house">
+        <h4>House</h4>
+        <h5>bedrooms</h5>
+        {house.bedrooms} 
+        <h5>bathrooms</h5>
+        {house.bathrooms}
+        
+        <h5>Data</h5>
+        <pre>{JSON.stringify(house, null, 2)}</pre>
+        <hr />
+      </div>
+      // the whole house printed ^ 
+    );
+  }
+  
