b3b6061f554df8683536f5557e11415005ca2c9c
Elaine <128241985+elainengg@users.noreply.github.com>
2024-10-20 13:23:38 -0400

    just need 3.8

--- src/Kanbas/Courses/People/Table.tsx
+++ src/Kanbas/Courses/People/Table.tsx
@@ -1,5 +1,16 @@
+
+
+import React from "react";
+import { useParams } from "react-router-dom";
+import * as db from "../../Database";
 import { FaUserCircle } from "react-icons/fa";
+
+
+
 export default function PeopleTable() {
+  const { cid } = useParams();
+  const { users, enrollments } = db;
+
   return (
 
     <div id="wd-people-table">
@@ -7,48 +18,30 @@ export default function PeopleTable() {
         <thead>
           <tr><th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th></tr>
         </thead>
+
+
+
         <tbody>
-          <tr><td className="wd-full-name text-nowrap">
-            <FaUserCircle className="me-2 fs-1 text-secondary" />
-            <span className="wd-first-name">Tony</span>{" "}
-            <span className="wd-last-name">Stark</span></td>
-            <td className="wd-login-id">001234561S</td>
-            <td className="wd-section">S101</td>
-            <td className="wd-role">STUDENT</td>
-            <td className="wd-last-activity">2020-10-01</td>
-            <td className="wd-total-activity">10:21:32</td> </tr>
-          {/* Add at least 3 more users such as Bruce Wayne, Steve Rogers, and Natasha Romanoff */}
-          <tr><td className="wd-full-name text-nowrap">
-            <FaUserCircle className="me-2 fs-1 text-secondary" />
-            <span className="wd-first-name">Bruce</span>{" "}
-            <span className="wd-last-name">Wayne</span></td>
-            <td className="wd-login-id">0012345616</td>
-            <td className="wd-section">S101</td>
-            <td className="wd-role">STUDENT</td>
-            <td className="wd-last-activity">2020-10-01</td>
-            <td className="wd-total-activity">10:21:37</td> </tr>
+  {users
+    .filter((usr) =>
+      enrollments.some((enrollment) => enrollment.user === usr._id && enrollment.course === cid)
+    )
+    .map((user: any) => (
+      <tr key={user._id}>
+        <td className="wd-full-name text-nowrap">
+          <FaUserCircle className="me-2 fs-1 text-secondary" />
+          <span className="wd-first-name">{user.firstName}</span>
+          <span className="wd-last-name">{user.lastName}</span>
+        </td>
+        <td className="wd-login-id">{user.loginId}</td>
+        <td className="wd-section">{user.section}</td>
+        <td className="wd-role">{user.role}</td>
+        <td className="wd-last-activity">{user.lastActivity}</td>
+        <td className="wd-total-activity">{user.totalActivity}</td>
+      </tr>
+    ))}
+</tbody>
 
-
-          <tr><td className="wd-full-name text-nowrap">
-            <FaUserCircle className="me-2 fs-1 text-secondary" />
-            <span className="wd-first-name">Steve</span>{" "}
-            <span className="wd-last-name">Rogers</span></td>
-            <td className="wd-login-id">0012345616</td>
-            <td className="wd-section">S101</td>
-            <td className="wd-role">STUDENT</td>
-            <td className="wd-last-activity">2020-10-01</td>
-            <td className="wd-total-activity">10:21:37</td> </tr>
-
-          <tr><td className="wd-full-name text-nowrap">
-            <FaUserCircle className="me-2 fs-1 text-secondary" />
-            <span className="wd-first-name">Natasha</span>{" "}
-            <span className="wd-last-name">Romanoff</span></td>
-            <td className="wd-login-id">0012345616</td>
-            <td className="wd-section">S101</td>
-            <td className="wd-role">STUDENT</td>
-            <td className="wd-last-activity">2020-10-01</td>
-            <td className="wd-total-activity">10:21:37</td> </tr>
-        </tbody>
       </table>
     </div>);
 }
