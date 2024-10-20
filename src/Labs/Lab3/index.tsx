d7ee8e1dd949cba2474f6dce836eaabbb88ea156
Elaine <128241985+elainengg@users.noreply.github.com>
2024-10-19 20:36:44 -0400

    2.5 im going to take a walk

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -31,6 +31,9 @@ import Destructing from "./Destructing"
 import FunctionDestructing from "./FunctionDestructing"
 import DestructingImports from "./DestructingImports"
 
+import Classes from "./Classes"
+import Styles from "./Styles"
+
 export default function Lab3() {
   return (
     <div id="wd-lab3">
@@ -62,6 +65,8 @@ export default function Lab3() {
       <Destructing/> 
       <FunctionDestructing/>
       <DestructingImports/>
+      <Classes/>
+      <Styles/>
     </div>
   );
 }
