04523ab82a24eba2d082faa34aed4dd832d64f94
Elaine <128241985+elainengg@users.noreply.github.com>
2024-10-20 01:20:19 -0400

    3.6

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -3,7 +3,7 @@ import { Link, useLocation, useParams } from "react-router-dom";
 
 export default function CoursesNavigation() {
   // need courseID from URL 
-  const { courseId } = useParams();
+  const { cid } = useParams();
   // path to class 
   const { pathname } = useLocation();
 
@@ -14,13 +14,14 @@ export default function CoursesNavigation() {
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
       {links.map((link) => {
-        const path = `/Kanbas/Courses/${courseId}/${link}`; // path for each link
+        const path = `/Kanbas/Courses/${cid}/${link}`; //  path for each link
+        const isActive = pathname.includes(link); // if the current link is active then make text-dark 
 
         return (
           <Link
             key={link}
             to={path}
-            className="list-group-item text-danger border border-0">
+            className={`list-group-item border border-0 ${isActive ? 'active text-dark' : 'text-danger'}`}>
             {link}
           </Link>
         );
