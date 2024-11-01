62e2d3a435d064391801fa7877d887c51e4d04c8
Elaine <128241985+elainengg@users.noreply.github.com>
2024-11-01 19:56:36 -0400

    2.3

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -2,6 +2,14 @@ import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions"; 
 import EventObject from "./EventObject"; 
+import Counter from "./Counter"; 
+import BooleanStateVariables from "./BooleanStateVariables"; 
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable"; 
+import ObjectStateVariable from "./ObjectStateVariable"; 
+import ArrayStateVariable from "./ArrayStateVariable"; 
+import ParentStateComponent from "./ParentStateComponent"; 
+import ChildStateComponent from "./ChildStateComponent";
 
 export default function Lab4() {
     function sayHello() {
@@ -14,10 +22,15 @@ export default function Lab4() {
       <ClickEvent />
       <PassingDataOnEvent/> 
       <PassingFunctions theFunction={sayHello} />
-
-
-
-
+      <EventObject/> 
+      <Counter/>
+      <BooleanStateVariables/> 
+      <StringStateVariables/>
+      <DateStateVariable/> 
+      <ObjectStateVariable/> 
+      <ArrayStateVariable/> 
+      <ParentStateComponent/> 
+      
       </div>);
 
 
