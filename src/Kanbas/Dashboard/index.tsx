3cc0e543d271ff801c3db78978ff5e4574abe6d1
Elaine <128241985+elainengg@users.noreply.github.com>
2024-11-03 16:55:54 -0500

    3.2

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,20 +1,50 @@
 import { Routes, Route, Navigate } from "react-router";
 import Dashboard from "./Dashboard";
 
-export default function What() {
+// defined an interface because the code was being annoying
+interface WhatProps {
+  courses: any[];
+  course: any;
+  setCourse: (course: any) => void;
+  addNewCourse: () => void;
+  deleteCourse: (courseId: any) => void;
+  updateCourse: () => void;
+}
+
+export default function What({
+  // so the type can be read
+  courses,
+  course,
+  setCourse,
+  addNewCourse,
+  deleteCourse,
+  updateCourse,
+}: WhatProps) {
   return (
     <div id="wd-account-screen">
       <table>
         <tr>
           <td valign="top">
-            <Dashboard />
+            <Dashboard
+              courses={courses}
+              course={course}
+              setCourse={setCourse}
+              addNewCourse={addNewCourse}
+              deleteCourse={deleteCourse}
+              updateCourse={updateCourse}
+            />
           </td>
           <td valign="top">
             <Routes>
-              <Route path="/"
-                element={<Navigate to="/Kanbas/Dashboard" />} />
-              <Route path="/Signin" element={<Dashboard />} />
-
+              <Route path="/" element={<Navigate to="/Kanbas/Dashboard" />} />
+              <Route path="/Signin" element={<Dashboard
+                courses={courses}
+                course={course}
+                setCourse={setCourse}
+                addNewCourse={addNewCourse}
+                deleteCourse={deleteCourse}
+                updateCourse={updateCourse}
+              />} />
             </Routes>
           </td>
         </tr>
