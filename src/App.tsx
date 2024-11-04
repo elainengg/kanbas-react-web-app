7c641704dd09dcc9cbc20ca802936817ad4f9f48
Elaine <128241985+elainengg@users.noreply.github.com>
2024-11-04 18:47:08 -0500

    Revert "3.3 total"
    This reverts commit bb78625d06ef0e72b84bcde8d1702994cb5f45e6.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import Labs from "./Labs";
 import Kanbas from "./Kanbas";
-import store from "./Kanbas/store";
-import { Provider } from "react-redux";
 
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 export default function App() {
   return (
     <HashRouter>
-            <Provider store={store}>
-
       <div>
         <Routes>
           <Route path="/" element={<Navigate to="Kanbas" />} />
@@ -16,8 +12,6 @@ export default function App() {
           <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
       </div>
-      </Provider>
-
     </HashRouter>
   );
 }
