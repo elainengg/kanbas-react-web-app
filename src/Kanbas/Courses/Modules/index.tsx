6e2bc6b858e5e285464fd37a57512ed37aa6922e
Elaine <128241985+elainengg@users.noreply.github.com>
2024-11-04 13:26:04 -0500

    3.3 total

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -5,37 +5,30 @@ import ModuleControlButtons from "./ModuleControlButtons";
 import { useParams } from "react-router";
 import * as db from "../../Database";
 import React, { useState } from "react";
+import { addModule, editModule, updateModule, deleteModule }
+  from "./reducer";
+import { useSelector, useDispatch } from "react-redux";
 
 export default function Modules() {
   const { cid } = useParams();
-  const [modules, setModules] = useState<any[]>(db.modules);
   const [moduleName, setModuleName] = useState("");
-  const addModule = () => {
-    setModules([ ...modules, { _id: new Date().getTime().toString(),
-                                     name: moduleName, course: cid, lessons: [] } ]);
-    setModuleName("");
-  };
-  const deleteModule = (moduleId: string) => {
-    setModules(modules.filter((m) => m._id !== moduleId));
-  };
-  const editModule = (moduleId: string) => {
-    setModules(modules.map((m) => (m._id === moduleId ? { ...m, editing: true } : m)));
-  };
-  const updateModule = (module: any) => {
-    setModules(modules.map((m) => (m._id === module._id ? module : m)));
-  };
-
+  const { modules } = useSelector((state: any) => state.modulesReducer);
+  const dispatch = useDispatch();
 
 
   return (
-    <div>
-
-      <ModulesControls setModuleName={setModuleName} moduleName={moduleName} addModule={addModule} />
+    <div className="wd-modules">
+      <ModulesControls moduleName={moduleName} setModuleName={setModuleName}
+        addModule={() => {
+          dispatch(addModule({ name: moduleName, course: cid }));
+          setModuleName("");
+        }} />
       <br /><br /><br /><br />
 
 
 
       <ul id="wd-modules" className="list-group rounded-0">
+        
         {modules
           .filter((module: any) => module.course === cid)
           .map((module: any) => (
@@ -63,7 +56,8 @@ export default function Modules() {
                 <ul className="wd-lessons list-group rounded-0">
                   {module.lessons.map((lesson: any) => (
                     <li className="wd-lesson list-group-item p-3 ps-1">
-                      <BsGripVertical className="me-2 fs-3" /> {lesson.name} <LessonControlButtons />
+                      <BsGripVertical className="me-2 fs-3" /> {lesson.name} 
+                      <LessonControlButtons />
                     </li>
                   ))}</ul>)}</li>))}</ul>
 
