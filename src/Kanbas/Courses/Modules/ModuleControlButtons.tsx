3d2a2a879fd16ff80f81b51fea7466d1cadd767d
Elaine Ng <128241985+elainengg@users.noreply.github.com>
2024-11-05 01:40:25 -0500

    3.3.2

--- src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
+++ src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
@@ -1,9 +1,14 @@
 import { IoEllipsisVertical } from "react-icons/io5";
 import GreenCheckmark from "./GreenCheckmark";
 import { FaPlus } from "react-icons/fa6";
-export default function ModuleControlButtons() {
+import { FaTrash } from "react-icons/fa";
+
+export default function ModuleControlButtons({ moduleId, deleteModule }: { 
+    moduleId: string; deleteModule: (moduleId: string) => void; } )  {
     return (
         <div className="float-end">
+                  <FaTrash className="text-danger me-2 mb-1" onClick={() => deleteModule(moduleId)}/>
+
             <GreenCheckmark />
             <IoEllipsisVertical className="fs-4" />
             <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
