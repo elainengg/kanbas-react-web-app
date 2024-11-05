9dc8698f8dd0366e57f98b622884a5e865fa80f1
Elaine Ng <128241985+elainengg@users.noreply.github.com>
2024-11-05 01:38:40 -0500

    3.3.1

--- src/Kanbas/Courses/Modules/ModulesControls.tsx
+++ src/Kanbas/Courses/Modules/ModulesControls.tsx
@@ -1,12 +1,16 @@
 import { FaPlus } from "react-icons/fa6";
 import GreenCheckmark from "./GreenCheckmark";
 import { MdDoNotDisturbAlt } from "react-icons/md";
-export default function ModulesControls() {
+
+import ModuleEditor from "./ModuleEditor";
+
+export default function ModulesControls({ moduleName, setModuleName, addModule }:
+    { moduleName: string; setModuleName: (title: string) => void; addModule: () => void; }) {
     return (
         <div id="wd-modules-controls" className="text-nowrap">
 
             <button id="wd-add-module-btn" className="btn btn-lg btn-danger me-1 float-end">
-                <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
+                <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} data-bs-toggle="modal" data-bs-target="#wd-add-module-dialog" />
                 Module</button>
 
 
@@ -53,6 +57,8 @@ export default function ModulesControls() {
             <button id="wd-collapse-all" className="btn btn-lg btn-secondary me-1 float-end">
 
                 Collapse All</button>
+                <ModuleEditor dialogTitle="Add Module" moduleName={moduleName}
+                    setModuleName={setModuleName} addModule={addModule} />
         </div>
     );
 }
