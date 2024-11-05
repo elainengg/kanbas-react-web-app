c2a3f5b96ec830d6111d9eaa08dfc42dbbadfdf7
Elaine Ng <128241985+elainengg@users.noreply.github.com>
2024-11-05 01:49:22 -0500

    3.3.4

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import Labs from "./Labs";
 import Kanbas from "./Kanbas";
+import store from "./Kanbas/store";
+import { Provider } from "react-redux";
+
 
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 export default function App() {
   return (
     <HashRouter>
+            <Provider store={store}>
       <div>
         <Routes>
           <Route path="/" element={<Navigate to="Kanbas" />} />
           <Route path="/Labs/*" element={<Labs />} />
           <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
-      </div>
+      </div> </Provider>
     </HashRouter>
   );
 }
--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -5,34 +5,23 @@ import ModuleControlButtons from "./ModuleControlButtons";
 import { useParams } from "react-router";
 import * as db from "../../Database";
 import { useState } from "react";
-
+import { addModule, editModule, updateModule, deleteModule }
+  from "./reducer";
+import { useSelector, useDispatch } from "react-redux";
 export default function Modules() {
   const { cid } = useParams();
-  const [modules, setModules] = useState<any[]>(db.modules);
   const [moduleName, setModuleName] = useState("");
-  const addModule = () => {
-    setModules([ ...modules, { _id: new Date().getTime().toString(),
-                                     name: moduleName, course: cid, lessons: [] } ]);
-    setModuleName("");
-  };
-
-  const deleteModule = (moduleId: string) => {
-    setModules(modules.filter((m) => m._id !== moduleId));
-  };
-  const editModule = (moduleId: string) => {
-    setModules(modules.map((m) => (m._id === moduleId ? { ...m, editing: true } : m)));
-  };
-  const updateModule = (module: any) => {
-    setModules(modules.map((m) => (m._id === module._id ? module : m)));
-  };
-
-
+  const { modules } = useSelector((state: any) => state.modulesReducer);
+  const dispatch = useDispatch();
 
   return (
     <div>
 
-      <ModulesControls setModuleName={setModuleName} moduleName={moduleName} addModule={addModule}/><br /><br /><br /><br />
-
+<ModulesControls moduleName={moduleName} setModuleName={setModuleName}
+        addModule={() => {
+          dispatch(addModule({ name: moduleName, course: cid }));
+          setModuleName("");
+        }} /><br /><br /><br /><br />
 
 
       <ul id="wd-modules" className="list-group rounded-0">
